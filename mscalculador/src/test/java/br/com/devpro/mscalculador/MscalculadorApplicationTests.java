a7530af449e54ce14c2237e75ebd61287eb2cf04
Alessandro <alessandronobre.ti@gmail.com>
2023-04-30 20:25:50 -0300

    subindo microserviço calculador

---
+++ mscalculador/src/test/java/br/com/devpro/mscalculador/MscalculadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpro.mscalculador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscalculadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
