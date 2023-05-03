a7bda11f6a4306697d6c8407ce24d22188664690
Alessandro <alessandronobre.ti@gmail.com>
2023-05-02 21:06:43 -0300

    Subindo demais configurações

---
+++ msgeradorpdf/src/main/java/br/com/devpro/msgeradorpdf/MsgeradorpdfApplication.java
@@ -0,0 +1,13 @@
+package br.com.devpro.msgeradorpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgeradorpdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgeradorpdfApplication.class, args);
+	}
+
+}
---
+++ msgeradorpdf/src/test/java/br/com/devpro/msgeradorpdf/MsgeradorpdfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpro.msgeradorpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgeradorpdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
