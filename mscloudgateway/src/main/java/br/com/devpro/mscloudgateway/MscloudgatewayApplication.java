2cca5cddfd859ba9e683bdec41d770d029b3a4a4
Alessandro <alessandronobre.ti@gmail.com>
2023-05-28 19:56:08 -0300

    mapeando rota

--- mscloudgateway/src/main/java/br/com/devpro/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/br/com/devpro/mscloudgateway/MscloudgatewayApplication.java
@@ -3,6 +3,9 @@ package br.com.devpro.mscloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -12,5 +15,13 @@ public class MscloudgatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
 	}
+
+    @Bean
+    RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/api/email/**").uri("lb://msemail"))
+                .build();
+    }
 
 }
